0aad5c1f6603104a39c45a83f428cc71d629b763
fukuball <fukuball@gmail.com>
2018-10-07 19:24:32 +0800

    Refine test, by use await asyc, and add more test case

--- test/test_splitter_ownerable.js
+++ test/test_splitter_ownerable.js
@@ -1,32 +1,38 @@
 var Splitter = artifacts.require('Splitter');
 
 contract('Splitter', function(accounts) {
-    it('should have the owner address equal to the sender', function() {
-        var contract;
-        var originalOwner = accounts[0];
-        return Splitter.deployed().then((instance) => {
-            contract = instance;
-            return contract.owner.call();
-        }).then((owner) => {
-            assert.equal(owner, originalOwner);
-        });
+    let catchRevert = require("./exceptions.js").catchRevert;
+
+    it('should have the owner address equal to the sender', async function() {
+        let originalOwner = accounts[0];
+        let contract = await Splitter.deployed();
+        let contractOwner = await contract.owner.call();
+        assert.equal(contractOwner, originalOwner);
     });
 
-    it('should have the ability to change owner and get the event', function() {
-        var contract;
-        var originalOwner = accounts[0];
-        var newOwner = accounts[1];
-        return Splitter.deployed().then((instance) => {
-            contract = instance;
-            return contract.changeOwner(newOwner, {from: originalOwner});
-        }).then((events) => {
-            assert.equal(events.logs.length, 1);
-            assert.equal(events.logs[0].event, 'LogChangeOwner');
-            assert.equal(events.logs[0].args.sender, originalOwner);
-            assert.equal(events.logs[0].args.newOwner, newOwner);
-            return contract.owner.call();
-        }).then((owner) => {
-            assert.equal(owner, newOwner);
-        });
+    it('should not change owner with original owner address', async function() {
+        let originalOwner = accounts[0];
+        let contract = await Splitter.deployed();
+        await catchRevert(contract.changeOwner(originalOwner, {from: originalOwner}));
+    });
+
+    it('should not change owner by not owner', async function() {
+        let originalOwner = accounts[0];
+        let other = accounts[1];
+        let contract = await Splitter.deployed();
+        await catchRevert(contract.changeOwner(other, {from: other}));
+    });
+
+    it('should have the ability to change owner and get the event', async function() {
+        let originalOwner = accounts[0];
+        let newOwner = accounts[1];
+        let contract = await Splitter.deployed();
+        let events = await contract.changeOwner(newOwner, {from: originalOwner});
+        assert.equal(events.logs.length, 1);
+        assert.equal(events.logs[0].event, 'LogChangeOwner');
+        assert.equal(events.logs[0].args.sender, originalOwner);
+        assert.equal(events.logs[0].args.newOwner, newOwner);
+        let contractOwner = await contract.owner.call();
+        assert.equal(contractOwner, newOwner);
     });
 });
